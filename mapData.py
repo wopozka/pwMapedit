56dec4ee356fe01fe719dd5cad86a048f9dfe9f7
Piotr Wawrzyniak <wopozka@gmail.com>
2023-07-25 15:16:41 +0200

    basic parameters setting/reading functions

--- mapData.py
+++ mapData.py
@@ -230,25 +230,47 @@ class mapObject(object):
 
 class BasicMapItem(QGraphicsItemGroup):
     def __init__(self, *args, **kwargs):
-        self.obj_data = OrderedDict({'Comment': list(), 'Type': '', 'Label': '', 'EndLevel': '',
-                                     'DataX': OrderedDict({})})
+        self.obj_data = OrderedDict({'Comment': list(), 'Type': '', 'Label': '', 'Label2': '', 'Label3': '',
+                                     'DirIndicator': bool, 'EndLevel': '', 'StreetDesc': '', 'CityIdx': '',
+                                     'DisctrictName': '', 'Phone': '', 'Highway': '',  'DataX': OrderedDict(),
+                                     'Others': OrderedDict()})
         self.obj_bounding_box = {}
         super(BasicMapItem, self).__init__(*args, **kwargs)
 
     def set_data(self, obj_data):
         for key in obj_data:
             if key == 'Comment':
-                self.obj_data[key] = [a for a in obj_data[key]]
-                continue
-            elif key in ('Type', 'Label', 'EndLevel', 'HouseNumber', 'StreetDesc', 'Phone', 'Highway'):
-                self.obj_data[key] = obj_data[key]
-                continue
+                self.obj_comment_set(obj_data[key])
+            elif key == 'Type':
+                self.obj_type_set(obj_data[key])
+            elif key == 'Label':
+                self.obj_label_set(obj_data[key])
+            elif key == 'Label2':
+                self.obj_label2_set(obj_data[key])
+            elif key == 'Label3':
+                self.obj_label3_set(obj_data[key])
+            elif key == 'DirIndicator':
+                self.obj_dirindicator_set(obj_data[key])
+            elif key == 'EndLevel':
+                self.obj_endlevel_set(obj_data[key])
+            elif key == 'StreetDesc':
+                self.obj_streetdesc_set(obj_data[key])
+            elif key == 'Phone':
+                self.obj_phone_set(obj_data[key])
+            elif key == 'Highway':
+                self.obj_highway_set(obj_data[key])
             elif key.startswith('Data0') or key.startswith('Data1') or key.startswith('Data2') \
-                    or key.startswith('Data3'):
-                self.obj_data['DataX'][key] = self.coord_from_data_to_point(obj_data[key])
-                continue
+                    or key.startswith('Data3') or key.startswith('Data4'):
+                self.obj_datax_set(key, obj_data[key])
             else:
                 self.obj_data['Other'][key] = obj_data[key]
+
+    def obj_comment_get(self):
+        return self.obj_data['Comment']
+
+    def obj_comment_set(self, _comments):
+        for _comment in _comments:
+            self.obj_data['Comment'].append(_comment)
 
     def obj_type_get(self):
         return self.obj_data['Type']
@@ -260,17 +282,65 @@ class BasicMapItem(QGraphicsItemGroup):
         return self.obj_data['Label']
 
     def obj_label_set(self, _label):
-        self.obj_data['Label'] = label
+        self.obj_data['Label'] = _label
 
-    def coords_from_data_to_points(self, Dataline):
-        coord = []
-        coordlist = Dataline.split('=')[-1]
+    def obj_label2_get(self):
+        return self.obj_data['Label2']
+
+    def obj_label2_set(self, _label):
+        self.obj_data['Label2'] = _label
+
+    def obj_label3_get(self):
+        return self.obj_data['Label3']
+
+    def obj_label3_set(self, _label):
+        self.obj_data['Label3'] = _label
+
+    def obj_dirindicator_get(self):
+        return self.obj_data['DirIndicator']
+
+    def obj_dirindicator_set(self, _dir):
+        self.obj_data['DirIndicator'] = _dir
+
+    def obj_endlevel_get(self):
+        return self.obj_data['EndLevel']
+
+    def obj_endlevel_set(self, _endlevel):
+        self.obj_data['EndLevel'] = _endlevel
+
+    def obj_streetdesc_get(self):
+        return self.obj_data['StreetDesc']
+
+    def obj_endlevel_set(self, _streetdesc):
+        self.obj_data['StreetDesc'] = _streetdesc
+
+    def obj_phone_get(self):
+        return self.obj_data['Phone']
+
+    def obj_phone_set(self, _phone):
+        self.obj_data['Phone'] = _phone
+
+    def obj_highway_get(self):
+        return self.obj_data['Highway']
+
+    def obj_phone_set(self, _highway):
+        self.obj_data['Highway'] = _highway
+
+    def obj_datax_get(self):
+        pass
+
+    def obj_datax_set(self, key, _dataX):
+        self.obj_data['DataX'][key] = self.coord_from_data_to_point(_dataX)
+
+    def coords_from_data_to_points(self, data_line):
+        coords = []
+        coordlist = data_line.split('=')[-1]
         coordlist = coordlist.strip().lstrip('(').rstrip(')')
         for a in coordlist.split('),('):
             latitude, longitude = a.split(',')
             self.set_obj_bounding_box(float(latitude), float(longitude))
-            coord.append(Point(latitude, longitude))
-        return coord
+            coords.append(Point(latitude, longitude))
+        return coords
 
     def set_obj_bounding_box(self, latitude, longitude):
         if not self.obj_bounding_box:
@@ -296,10 +366,6 @@ class BasicMapItem(QGraphicsItemGroup):
 
 class POI(BasicMapItem):
     def __init__(self, obj_data):
-        _obj_data = {'Comment': list(), 'Type': '', 'Label': '', 'EndLevel': '', 'HouseNumber': '', 'StreetDesc': '',
-                     'Phone': '', 'DataX': OrderedDict({}), 'Highway': '', 'EntryPoint': OrderedDict(),
-                     'Other': OrderedDict({})}
-        self.obj_data = OrderedDict(_obj_data)
         super(POI, self).__init__()
         self.set_data(obj_data)
 
