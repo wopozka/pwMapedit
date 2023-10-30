51e611d2c32af0e09464243138f7f8ef19610cc0
Piotr Wawrzyniak <wopozka@gmail.com>
2023-10-30 15:45:58 +0100

    wreszcie rysuje cos na mapie! set_canvas_rectangle bylo problemem. Teraz tylko dalsze kroki.

--- pwMapeEdit.py
+++ pwMapeEdit.py
@@ -185,6 +185,7 @@ class pwMapeditPy(QMainWindow):
             self.map_objects.wczytaj_rekordy()
             print(self.map_canvas.sceneRect())
             self.map_canvas.draw_all_objects_on_map(self.map_objects.get_all_map_objects(), 'Data0')
+            self.map_canvas.set_canvas_rectangle(self.map_objects.get_map_bounding_box())
             # print(self.map_canvas.sceneRect())
             # print(self.map_canvas.itemsBoundingRect())
             # self.view.fitInView(self.map_canvas.itemsBoundingRect(), Qt.KeepAspectRatio)
