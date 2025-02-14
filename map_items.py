1a2d4673c6c99172a9a9fc15e0c976a96ce9d307
Piotr Wawrzyniak <wopozka@gmail.com>
2025-02-14 19:10:57 +0100

    zaczynamy ogarniac numeracje

--- map_items.py
+++ map_items.py
@@ -759,14 +759,11 @@ class Data_X(object):
                 poly_points.append(data_x + ','.join([point.get_mp_coords(self.precision) for point in poly]))
                 nodes_with_nums = [(node_num, node) for node_num, node in enumerate(poly) if node.node_has_numeration()]
                 if len(nodes_with_nums) >= 2:
+                    numbers_defs = []
                     for node_pair in itertools.pairwise(nodes_with_nums):
-                        start_node = node_pair[0]
-                        end_node = node_pair[1]
-                        if start_node.node_starts_numeration():
-                            left_def = ''
-                        else:
-                            left_def = 'N,-1,-1'
-                            right_def = 'N,-1,-1'
+                        numbers_defs.append(self.numbers_to_mp(node_pair[0], node_pair[1]))
+                    for num, number_def in enumerate(numbers_defs):
+                        poly_points.append('Numbers' + str(num) + '=' + str(poly.index(nodes_with_nums[num])))
 
 
 
@@ -775,6 +772,25 @@ class Data_X(object):
 
 
         return poly_points
+
+    def numbers_to_mp(self, start_node, end_node):
+        # tworzenie definicji numeracji zgodnej z plikiem mp na podstawie pary nodow
+        s_numeration = start_node[1].get_numbers_definition()
+        e_numeration = end_node[1].get_numbers_definition()
+        numeration = ''
+        if s_numeration.left_side_numbering_style is not None and s_numeration.left_side_numbering_style != 'N'
+            numeration += s_numeration.left_side_numbering_style
+            numeration += s_numeration.left_side_number_after
+            numeration += e_numeration.left_side_number_before
+        else:
+            numeration += 'N,-1,-1'
+        if s_numeration.right_side_numbering_style is not None and s_numeration.left_side_numbering_style != 'N'
+            numeration += s_numeration.right_side_numbering_style
+            numeration += s_numeration.right_side_number_after
+            numeration += e_numeration.right_side_number_before
+        else:
+            numeration += 'N,-1,-1'
+        return str(start_node[0]) + ',' + numeration
 
     def update_node_coordinates(self, data_level, polynum, index, position):
         """
