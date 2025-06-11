98da420457f89690fa33a1b76c102248968059fb
wopozka <wopozka@gmail.com>
2025-06-11 14:10:21 +0200

    qt6

--- map_obj_properties_dockwidget.py
+++ map_obj_properties_dockwidget.py
@@ -1016,8 +1016,8 @@ class NumberEdit(QLineEdit):
                     self.set_valid()
                 else:
                     self.set_invalid()
-        print(event.key(), Qt.Key_Enter)
-        if event.key() == Qt.Key_Enter or event.key() == Qt.Key_Return:
+        print(event.key(), Qt.Key.Key_Enter)
+        if event.key() == Qt.Key.Key_Enter or event.key() == Qt.Key.Key_Return:
             self.number_edited()
 
     def set_invalid(self):
